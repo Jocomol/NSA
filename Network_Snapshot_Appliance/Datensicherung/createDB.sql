a1bb2f31f68a9a3a1b50fbb7dc6fe5e91dc6ba94
Jocomol <joelmeier08@gmail.com>
2018-10-30 08:45:27 +0100

    Changed createDB.sql

--- Network_Snapshot_Appliance/Datensicherung/createDB.sql
+++ Network_Snapshot_Appliance/Datensicherung/createDB.sql
@@ -1,10 +1,10 @@
 CREATE TABLE dns(
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 working INTEGER
 );
 
 CREATE TABLE dhcp(
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 leastime INTEGER,
 ip_offered TEXT,
 subnetmask TEXT,
@@ -13,36 +13,38 @@ domainname TEXT
 );
 
 CREATE TABLE role(
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 role TEXT
 );
 
+CREATE TABLE query(
+id INTEGER PRIMARY KEY NOT NULL,
+date INTEGER NOT NULL,
+wanconnection INTEGER NOT NULL,
+domainname TEXT NOT NULL,
+match INTEGER
+);
+
 CREATE TABLE ip(
-id INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY NOT NULL,
 ip TEXT,
 hostname TEXT,
 devicetype TEXT,
 deviceos TEXT,
 dhcp_id INTEGER,
 dns_id INTEGER,
+query_id INTEGER,
+FOREIGN KEY (query_id) REFERENCES query(id),
 FOREIGN KEY (dhcp_id) REFERENCES dhcp(id),
 FOREIGN KEY (dns_id) REFERENCES dns(id)
 );
 
 CREATE TABLE ip_role(
-id INTEGER PRIMARY KEY ,
+id INTEGER PRIMARY KEY,
 ip_id INTEGER,
 role_id INTEGER,
 FOREIGN KEY (ip_id) REFERENCES ip(id),
 FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-CREATE TABLE query(
-id INTEGER PRIMARY KEY,
-date INTEGER,
-ip_id INTEGER,
-wanconnection INTEGER,
-domainname TEXT,
-match INTEGER,
-FOREIGN KEY (ip_id) REFERENCES ip(id)
-);
+
