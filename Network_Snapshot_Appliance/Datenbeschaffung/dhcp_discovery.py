6d03d9fa1499ae3019f271ca47497cf9e5b00178
Joe Meier <joelmeier08@gmail.com>
2018-11-13 11:02:29 +0100

    Resolved error when it isn't able to resolve

--- Network_Snapshot_Appliance/Datenbeschaffung/dhcp_discovery.py
+++ Network_Snapshot_Appliance/Datenbeschaffung/dhcp_discovery.py
@@ -6,16 +6,21 @@ def run():
     OneDHCP=[]
     parsedDHCP=[]
     for i in range(0, 10):
-        output = subprocess.check_output(["nmap", "--script=broadcast-dhcp-discover"])
+        #output = subprocess.check_output(["nmap", "--script=broadcast-dhcp-discover"])
+        output = subprocess.check_output(["cat", "output.txt"])
         for c in range(0, 9):
             if c != 2 and c != 0:
                 helpme = (str(output).split("\\n")[c].split(":")[1].strip())
                 OneDHCP.append(helpme)
 
         parsedDHCP.append(OneDHCP[1])                   #0
-        domainname = subprocess.check_output(["nslookup", OneDHCP[1]]) #1
-        domainname = str(domainname).split(".\\n")[0].split("=")[1]
-        parsedDHCP.append(domainname)
+        try:
+            domainname = subprocess.check_output(["nslookup", OneDHCP[1]]) #1
+            domainname = str(domainname).split(".\\n")[0].split("=")[1]
+        except Exception:
+            parsedDHCP.append(None)
+        else:
+            parsedDHCP.append(domainname)
         parsedDHCP.append(None)                         #2
         parsedDHCP.append(None)                         #3
         parsedDHCP.append(OneDHCP[2])                   #4
@@ -28,3 +33,5 @@ def run():
         dhcpList.append(parsedDHCP)
 
     return dhcpList
+
+print(run())
