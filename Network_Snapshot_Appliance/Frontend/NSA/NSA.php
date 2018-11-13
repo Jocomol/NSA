a4dfdf1a3c0fbb6fcab29064b27278e7203a2f29
laxushan <44192128+laxushan@users.noreply.github.com>
2018-11-13 10:32:44 +0100

    Webseite mit Login und SQL

---
+++ Network_Snapshot_Appliance/Frontend/NSA/NSA.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
+    <title></title>
+
+  </head>
+  <body>
+
+<?php
+    $db = new PDO('sqlite:C:\xampp\htdocs\xampp\NSA\NSA_DB.db'); //Pfad muss noch angepasst werden wenn es auf einer Linux Syszem läuft
+?>
+
+
+<div class="info">
+  <h4>Internet: <?php
+  $internet =$db->query("SELECT wanconnection FROM query")->fetchColumn();
+  echo $internet;
+  ?></h4>
+  <h4>Doamin: <?php
+  $domain =$db->query("SELECT domainname FROM query")->fetchColumn();
+  echo $domain;
+  ?></h4>
+  <h4>Netmask: <?php
+  $netmask =$db->query("SELECT subnetmask FROM dhcp")->fetchColumn();
+  echo $netmask;
+  ?></h4>
+</div>
+<div class="tabellen">
+<div class="tablerechts">
+<?php
+        $sql = "SELECT * FROM ip";
+      echo '
+<table class="table table-bordered table-striped">
+        <tr>
+            <td>ID</td>
+            <td>IP</td>
+            <td>Hostname</td>
+            <td>Devicetype</td>
+            <td>DNS</td>
+            <td>DHCP</td>
+            <td>Query</td>
+        </tr>
+    </table>';
+        foreach ($db->query($sql) as $row)
+        {
+
+        echo '<table class="table table-bordered table-striped">
+                <tr>
+                    <td>'.$row['id'].'</td>
+                    <td>'.$row['ip'].'</td>
+                    <td>'.$row['hostname'].'</td>
+                    <td>'.$row['devicetype'].'</td>
+                    <td>'.$row['dns_id'].'</td>
+                    <td>'.$row['dhcp_id'].'</td>
+                    <td>'.$row['query_id'].'</td>
+                </tr>
+            </table>';
+        }
+?>
+<br>
+<?php
+        $sql = "SELECT * FROM dhcp";
+      echo '
+<table class="table table-bordered table-striped">
+        <tr>
+            <td>DHCP</td>
+            <td>IP</td>
+            <td>ON / OFF</td>
+        </tr>
+    </table>';
+        foreach ($db->query($sql) as $row)
+        {
+
+        echo '<table class="table table-bordered table-striped">
+                <tr>
+                    <td>'.$row['id'].'</td>
+                    <td>'.$row['ip_offered'].'</td>
+                    <td>'.$row['router'].'</td>
+                </tr>
+            </table>';
+        }
+?>
+</div>
+<br>
+<div id="tablelinks">
+<?php
+        $sql = "SELECT * FROM dns";
+      echo '
+<table class="table table-bordered table-striped">
+        <tr>
+            <td>DNS</td>
+            <td>IP</td>
+            <td>ON / OFF</td>
+        </tr>
+    </table>';
+        foreach ($db->query($sql) as $row)
+        {
+
+        echo '<table class="table table-bordered table-striped">
+                <tr>
+                    <td>'.$row['id'].'</td>
+                    <td>'.$row['working'].'</td>
+                    <td>'.$row['working'].'</td>
+                </tr>
+            </table>';
+        }
+?>
+</div>
+
+</div>
+<div class="ButtonScan">
+<button type="button" name="ScanButton" >Scan</button>
+</div>
+  </body>
+</html>
--- Network_Snapshot_Appliance/Frontend/NSA/index.php
+++ Network_Snapshot_Appliance/Frontend/NSA/index.php
@@ -1,68 +1,118 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link href="style.css" rel="stylesheet">
-    <title></title>
-
-  </head>
-  <body>
-
 <?php
-    $db = new PDO('sqlite:C:\xampp\htdocs\xampp\NSA\NSA_DB.db');
+   ob_start();
+   session_start();
 ?>
 
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
 
+<html lang = "en">
 
-<div class="links">
-  <h4>Internet:(PHP) </h4>
-  <h4>Doamin(php) </h4>
-  <h4>Netmask(php) </h4>
-</div>
-<div class="vl"></div>
+   <head>
+      <title>Tutorialspoint.com</title>
+      <link href = "css/bootstrap.min.css" rel = "stylesheet">
 
-<div class="TableClient">
-<table>
-  <tr>
-    <td>(php)CLIENT</td>
-    <td>IP</td>
-    <td>Typ</td>
-    <td>OS</td>
-  </tr>
-  <td>    <?php
-          $result =$db->query("SELECT count(*) FROM ip")->fetchColumn();
-          echo $result;
-      ?></td>
+      <style>
+         body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #ADABAB;
+         }
 
-    <td>
-      <?php
-              $results =$db->query("SELECT ip FROM ip WHERE ID='5'")->fetchColumn();
-              echo $results;
-          ?>
-    </td>
-</table>
-</div>
-<br>
-<div class="TableDNS">
-  <table>
-    <tr>
-      <td>(php)DNS</td>
-    </tr>
-  </table>
-</div>
+         .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: #017572;
+         }
 
-<div class="TableIP">
-  <table>
-    <tr>
-      <td>(php)ip</td>
-    </tr>
-  </table>
-</div>
-<div class="ButtonScan">
-<button type="button" name="ScanButton">Scan</button>
-</div>
-<div class="ButtonDownload">
-  <button type="button" name="DownloadButton">Download</button>
-</div>
-  </body>
+         .form-signin .form-signin-heading,
+         .form-signin .checkbox {
+            margin-bottom: 10px;
+         }
+
+         .form-signin .checkbox {
+            font-weight: normal;
+         }
+
+         .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+         }
+
+         .form-signin .form-control:focus {
+            z-index: 2;
+         }
+
+         .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+         }
+
+         .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+         }
+
+         h2{
+            text-align: center;
+            color: #017572;
+         }
+      </style>
+
+   </head>
+
+   <body>
+
+      <div class = "container form-signin">
+
+         <?php
+            $msg = '';
+
+            if (isset($_POST['login']) && !empty($_POST['username'])
+               && !empty($_POST['password'])) {
+
+               if ($_POST['username'] == 'NSA' &&
+                  $_POST['password'] == 'sml12345') {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'tutorialspoint';
+
+                  header("location: NSA.php");
+               }else {
+                  $msg = 'Wrong username or password';
+               }
+            }
+         ?>
+      </div> <!-- /container -->
+
+      <div class = "container">
+
+         <form class = "form-signin" role = "form"
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control"
+               name = "username" placeholder = "username"
+               required autofocus></br>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "password" required>
+               <br>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit"
+               name = "login">Login</button>
+         </form>
+      </div>
+
+   </body>
 </html>
