9e9a04b621adc88b0774abad78c2c5c889c7542c
Joe Meier <joelmeier08@gmail.com>
2018-11-13 09:39:19 +0100

    finished dhcp_discovery script

--- Network_Snapshot_Appliance/Datenbeschaffung/dhcp_discovery.py
+++ Network_Snapshot_Appliance/Datenbeschaffung/dhcp_discovery.py
@@ -1,30 +1,30 @@
-##WIP
+#!/usr/bin/python3.5
 import subprocess
 import re
-dhcpList=[]
-for i in range(0, 1):
-    ##output = subprocess.check_output(["nmap", "--script=broadcast-dhcp-discover"])
-    output = subprocess.check_output(["cat", "output.txt"])
+def run():
+    dhcpList=[]
+    OneDHCP=[]
+    parsedDHCP=[]
+    for i in range(0, 10):
+        output = subprocess.check_output(["nmap", "--script=broadcast-dhcp-discover"])
+        for c in range(0, 9):
+            if c != 2 and c != 0:
+                helpme = (str(output).split("\\n")[c].split(":")[1].strip())
+                OneDHCP.append(helpme)
 
-    for c in range(0, 9):
-        if c != 2 and c != 0:
-            print (str(output).split("\\n")[c].split(":")[1].strip())
+        parsedDHCP.append(OneDHCP[1])                   #0
+        domainname = subprocess.check_output(["nslookup", OneDHCP[1]]) #1
+        domainname = str(domainname).split(".\\n")[0].split("=")[1]
+        parsedDHCP.append(domainname)
+        parsedDHCP.append(None)                         #2
+        parsedDHCP.append(None)                         #3
+        parsedDHCP.append(OneDHCP[2])                   #4
+        parsedDHCP.append(OneDHCP[0])                   #5
+        parsedDHCP.append(OneDHCP[3])                   #6
+        parsedDHCP.append(OneDHCP[4])                   #7
+        parsedDHCP.append(OneDHCP[6])                   #8
+        parsedDHCP.append(None)                         #9
 
-    # TODO fill out arrays
-    #for e in range (0,10):
-    #    arrayindex = 0
-    #    if e != 2 and c != 3 and c != 9:
-    #        print
-    #offered_ip = str(output).split("\\n")[1].split(":")[1].strip()
-    #ip = str(output).split("\\n")[3].split(":")[1].strip()
+        dhcpList.append(parsedDHCP)
 
-
-#Notes  2,3,9 not 
-#     ["192.168.1.2","dhcp",None,None,"2","192.681.22.3","255.0.0.0","192.168.1.1","local",None],
-#ip offered = 1
-#ip dhcp = 3
-#leastime = 4
-#mask = 5
-#router = 6
-#dns = 7
-#domain name = 8
+    return dhcpList
