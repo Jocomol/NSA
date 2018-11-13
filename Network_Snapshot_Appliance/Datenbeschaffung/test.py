f8dfbe3257911ea401c69f29c43b0cb8846c5e90
svtr2000 <svtr2000@gmail.com>
2018-11-13 11:27:31 +0100

    more tests

--- Network_Snapshot_Appliance/Datenbeschaffung/test.py
+++ Network_Snapshot_Appliance/Datenbeschaffung/test.py
@@ -1,51 +1,64 @@
 #!/usr/bin/python3.5
 #Testfile
 
-#import ipaddress
+import subprocess, ipaddress
+from subprocess import Popen, PIPE
 
-#def getNetmask():
-#    ipaddress.ipnetwork
-
-
-#text = getNetmask()
-#print(text)
-
-##wenn response == 0 ist IP erreichbar
-#Quelle: https://stackoverflow.com/questions/2953462/pinging-servers-in-python
-#import os
-
-#ip = "8.8.8.8"
-#response = os.system("ping -c 1 " + ip)
-#print(str(response))
-
-
-
-#parsing
-
-#import subprocess
-
-
-#output = subprocess.check_output(["ifconfig"]).decode()
-
-#array = output.split('\n')
-
-#for item in array:
-    #if "inet addr" in item:
-        #if "addr:127" not in item:
-            #line = item.split(" ")
-
-#for part in line:
-    #if "addr" in part:
-        #ip = part.split(":")[1]
-    #if "Mask" in part:
-        #mask = part.split(":")[1]
-
+def getIP():
     
-#combined = ip + "/" + mask
+    ##TODO, Adresse von Host erhalten z.B. 192.168.220.0/255.255.255.0
+    
+    output = subprocess.check_output(["ifconfig"]).decode()
 
-#return combined
+    array = output.split('\n')
+
+    for item in array:
+        if "inet addr" in item:
+            if "addr:127" not in item:
+                line = item.split(" ")
+
+    for part in line:
+        if "addr" in part:
+            ip = part.split(":")[1]
+        if "Mask" in part:
+            mask = part.split(":")[1]
+            
+    netid = getNetID(ip, mask)
+    
+    combined = netid + "/" + mask
+
+    return combined
+
+def getNetID():
+    
+    
+    return netid
+
+def pingHosts(ip):
+    #Code von: https://topnetworkguide.com/how-to-use-python-to-ping-all-ip-addresses-on-a-network/ vom 13.11.2018
+    #Wurde modifiziert
+    ips = []
+    network = ipaddress.ip_network(ip)
+    for i in network.hosts():
+        i = str(i)
+        toping = Popen(['ping','-c', '1', i], stdout=PIPE)
+        output = toping.communicate()[0]
+        hostalive = toping.returncode
+        if hostalive == 0:
+            #reachable
+            #2D-Array
+            #[["192.168.1.1", "client",None,None,None,None,None,None,None,None],["192.168.1.2", "client",None,None,None,None,None,None,None,None]]
+            try:
+                #Domainname herausfinden
+                domainname = subprocess.check_output(["nslookup", i]) #1
+                domainname = str(domainname).split(".\\n")[0].split("=")[1]
+            except subprocess.CalledProcessError:
+                domainname = ""
+            ip = [str(i),domainname,None,None,None,None,None,None,None,None]
+            ips.append(ip)
+    return ips
 
 
-import scriptlib
+#print(pingHosts("192.168.210.5/255.255.255.0"))
 
-print(scriptlib.ipDiscovery())
+print(getIP())
